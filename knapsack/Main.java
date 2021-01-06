28df5d4e4c9c10f13e0817ec460578da2356205b
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:03:01 +0300

    Create Main.java

---
+++ knapsack/Main.java
@@ -0,0 +1,6 @@
+Bag secondBag = new Bag(30);
+
+effectiveFillBackpack(secondBag, items);
+
+System.out.println("Вес рюкзака составляет - " + secondBag.getCurrentWeight() +
+       ", общая стоимость вещей в рюкзаке - " + secondBag.getCurrentCost());
