cd6ab9291b9f1b3836066f08e94b3c4598aa3a96
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:17:18 +0300

    Create MainClass.java

---
+++ NOK_NOD/MainClass.java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two integers: ");
+
+        try {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.printf("Sum: %d; Difference: %d;\nGreatest Common Divisor: %d;\nLeast Common Multiple: %d.",
+                    Program.sum(a, b), Program.diff(a, b), Program.gcd(a, b), Program.lcm(a, b));
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
