8310a0d94c0dfd2aa41b36ee767b2514b5e0d89c
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:24:33 +0300

    Create Program.java

---
+++ Task_4/Program.java
@@ -0,0 +1,20 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Program {
+    //not case sensitive
+    static int getWordCount(String text, String word) {
+        if (word.equals("") || text.equals("")) throw new IllegalArgumentException("Invalid input");
+
+        text = text.toLowerCase();
+        word = word.toLowerCase();
+
+        int wordCount = 0;
+        Matcher matcher = Pattern.compile("\\b" + word + "\\b").matcher(text);
+
+        while (matcher.find()) {
+            wordCount++;
+        }
+        return wordCount;
+    }
+}
