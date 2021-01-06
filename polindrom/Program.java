7e372de46079069ca1f864157a29d30f5b8511c1
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:11:37 +0300

    Create Program.java

---
+++ polindrom/Program.java
@@ -0,0 +1,13 @@
+class Program {
+    static boolean isPalindrome(int i) {
+        int next = 0;
+        int temp = i;
+
+        while (temp != 0) {
+            next = (next * 10) + (temp % 10);
+            temp /= 10;
+        }
+
+        return i == next;
+    }
+}
