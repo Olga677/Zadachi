8c684117d08b7936d6def62962f4455a8f739f3a
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:21:02 +0300

    Create MainClass.java

---
+++ Task _3/MainClass.java
@@ -0,0 +1,22 @@
+import common.ConsoleReader;
+
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Enter the sentence: ");
+
+        try {
+            String sentence = ConsoleReader.readSentence();
+
+            List<String> words = Program.getWords(sentence);
+            System.out.println("Count of words: " + words.size());
+            Program.sort(words);
+
+            System.out.println(words);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
