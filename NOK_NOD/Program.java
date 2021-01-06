7c96aef55e3ea9bc3ce6dd7ae919d8a0b5e3d1c3
Olga677 <64420427+Olga677@users.noreply.github.com>
2021-01-07 00:16:40 +0300

    Create Program.java

---
+++ NOK_NOD/Program.java
@@ -0,0 +1,19 @@
+class Program {
+    static int gcd(int a, int b) {
+        if (a == 0 && b == 0) throw new IllegalArgumentException("lcm(a ,b). Parameters a and b are equals to zero.");
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    static int lcm(int a, int b) {
+        if (a == 0 && b == 0) throw new IllegalArgumentException("lcm(a ,b). Parameters a and b are equals to zero.");
+        return a / gcd(a, b) * b;
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int diff(int a, int b) {
+        return a - b;
+    }
+}
